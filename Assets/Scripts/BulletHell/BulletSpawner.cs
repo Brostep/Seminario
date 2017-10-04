e942228513c152985d51879c7b159410638e3816
Cracktos <falzarano.alejandro@gmail.com>
2017-10-04 17:52:23 -0300

    update 04/10/2017

--- Assets/Scripts/BulletHell/BulletSpawner.cs
+++ Assets/Scripts/BulletHell/BulletSpawner.cs
@@ -2,52 +2,50 @@ using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-	public AnimationCurve curve;
+    public AnimationCurve curve;
 
-	public HellBullet prefab;
+    public HellBullet prefab;
 
-	public Transform[] spawnPoints;
+    public Transform[] spawnPoints;
 
-	public float timeLapse;
+    public float timeLapse;
 
-	public float rotationAngle;
+    public float rotationAngle;
+    public float rotationSpeed;
+    public float rotationOffset;
 
-	public float rotationSpeed;
+    private float currentTime;
 
-	private float currentTime;
+    private int lastIndex;
 
-	private int lastIndex;
+    void Start()
+    {
+        lastIndex = -1;
+    }
 
-	void Start()
-	{
-		lastIndex = -1;
-	}
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(0.0f, rotationAngle * Mathf.Sin(Time.time * rotationSpeed) - rotationOffset, 0.0f);
 
-	void Update()
-	{
-		transform.rotation = Quaternion.Euler(0.0f, rotationAngle * Mathf.Sin(Time.time * rotationSpeed) - 180, 0.0f);
+        if (currentTime < timeLapse)
+        {
+            var t = currentTime / timeLapse;
 
-		if (currentTime < timeLapse)
-		{
-			var t = currentTime / timeLapse;
+            var index = Mathf.FloorToInt(spawnPoints.Length * curve.Evaluate(t));
 
-			var index = Mathf.FloorToInt(spawnPoints.Length * curve.Evaluate(t));
+            if (index != lastIndex)
+            {
+                Instantiate(prefab, spawnPoints[index].transform.position, spawnPoints[index].transform.rotation);
 
-			if (index != lastIndex)
-			{
-				Instantiate(prefab, spawnPoints[index].transform.position, spawnPoints[index].transform.rotation);
+                lastIndex = index;
+            }
+        }
 
-				lastIndex = index;
+        else
+        {
+            currentTime = default(float);
+        }
 
-				// Debug.Log(index);
-			}
-		}
-
-		else
-		{
-			currentTime = 0.0f;
-		}
-
-		currentTime += Time.deltaTime;
-	}
+        currentTime += Time.deltaTime;
+    }
 }
