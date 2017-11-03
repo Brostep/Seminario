417fe8b7c2946a3e2ccb077f42b953a46e675aa2
Brostep <ltr.pereyra@gmail.com>
2017-11-03 02:41:31 -0300

    Revert "Merge branch 'Player-Movement' of https://github.com/Brostep/Seminario into Player-Movement"
    This reverts commit 33091a5c0c0d5aac5d80d5b487b37ebd86276f6d, reversing
    changes made to 9e96e2014b8289f7dfdd41de57bd42b600f0013c.

--- Assets/Scripts/BulletHell/HellBulletSpawner.cs
+++ Assets/Scripts/BulletHell/HellBulletSpawner.cs
@@ -14,71 +14,63 @@ public class HellBulletSpawner : MonoBehaviour
     public float rotationAngleY;
     public float rotationSpeed;
     public float rotationOffset;
-
-    [HideInInspector]
-    public Vector3 spawnPosition;
-
-    [HideInInspector]
-    public Quaternion spawnRotation;
-
-    [HideInInspector]
-    public bool inRoom;
-
+	[HideInInspector]
+	public Vector3 spawnPosition;
+	[HideInInspector]
+	public Quaternion spawnRotation;
+	public bool inRoom;
     private float currentTime;
 
     private int lastIndex;
 
-    private Pool<HellBullet> _hellBulletPool;
+	private Pool<HellBullet> _hellBulletPool;
 
-    private static HellBulletSpawner _instance;
+	private static HellBulletSpawner _instance;
+	public static HellBulletSpawner Instance { get { return _instance; } }
 
-    public static HellBulletSpawner Instance { get { return _instance; } }
-
-    void Start()
+	void Start()
     {
-        _instance = this;
-        _hellBulletPool = new Pool<HellBullet>(80, BulletFactory, hellBulletPrefab.InitializePool, hellBulletPrefab.DisposePool, true);
-        lastIndex = -1;
+		_instance = this;
+		_hellBulletPool = new Pool<HellBullet>(80, BulletFactory, hellBulletPrefab.InitializePool, hellBulletPrefab.DisposePool, true);
+		lastIndex = -1;
     }
 
     void Update()
     {
-        if (inRoom)
-        {
-            transform.rotation = Quaternion.Euler(rotationAngleX * Mathf.Sin(Time.time * rotationSpeed), rotationAngleY * Mathf.Sin(Time.time * rotationSpeed) - rotationOffset, 0.0f);
+		if (inRoom)
+		{
+			transform.rotation = Quaternion.Euler(rotationAngleX * Mathf.Sin(Time.time * rotationSpeed), rotationAngleY * Mathf.Sin(Time.time * rotationSpeed) - rotationOffset, 0.0f);
 
-            if (currentTime < timeLapse)
-            {
-                var t = currentTime / timeLapse;
+			if (currentTime < timeLapse)
+			{
+				var t = currentTime / timeLapse;
 
-                var index = Mathf.FloorToInt(spawnPoints.Length * curve.Evaluate(t));
+				var index = Mathf.FloorToInt(spawnPoints.Length * curve.Evaluate(t));
 
-                if (index != lastIndex)
-                {
-                    _hellBulletPool.GetObjectFromPool();
-                    spawnPosition = spawnPoints[index].transform.position;
-                    spawnRotation = spawnPoints[index].transform.rotation;
+				if (index != lastIndex)
+				{
+					_hellBulletPool.GetObjectFromPool();
+					spawnPosition = spawnPoints[index].transform.position;
+					spawnRotation = spawnPoints[index].transform.rotation;
 
-                    lastIndex = index;
-                }
-            }
+					lastIndex = index;
+				}
+			}
+			else
+			{
+				currentTime = default(float);
+			}
 
-            else
-            {
-                currentTime = default(float);
-            }
-
-            currentTime += Time.deltaTime;
-        }
+			currentTime += Time.deltaTime;
+		}  
     }
+	private HellBullet BulletFactory()
+	{
+		return Instantiate<HellBullet>(hellBulletPrefab);
+	}
 
-    private HellBullet BulletFactory()
-    {
-        return Instantiate<HellBullet>(hellBulletPrefab);
-    }
-
-    public void ReturnBulletToPool(HellBullet bullet)
-    {
-        _hellBulletPool.DisablePoolObject(bullet);
-    }
+	public void ReturnBulletToPool(HellBullet bullet)
+	{
+		_hellBulletPool.DisablePoolObject(bullet);
+	}
 }
