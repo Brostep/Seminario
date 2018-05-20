fc5f9dfe10777c9cb14bcc14efa2e4661257dcc8
Cracktos <falzarano.alejandro@gmail.com>
2018-05-20 03:36:42 -0300

    Revert "Revert " new stuff""
    This reverts commit fd351cfa2c5f1465be4d18a97830b54d07856fa2.

---
+++ Assets/AuxiliaryPassageBehaviour.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class AuxiliaryPassageBehaviour : MonoBehaviour
+{
+    public GameObject passage;
+    public int triggerPoints;
+
+    private void Start()
+    {
+        if (passage.GetComponent<Rigidbody>().useGravity)
+            passage.GetComponent<Rigidbody>().useGravity = false;
+    }
+
+    private void Update()
+    {
+        if (triggerPoints <= 0)
+        {
+            GetComponent<MeshRenderer>().gameObject.SetActive(false);
+            GetComponent<MeshCollider>().gameObject.SetActive(false);
+            passage.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collider)
+    {
+        if (collider.gameObject.layer == 9)
+            triggerPoints--;
+    }
+}
